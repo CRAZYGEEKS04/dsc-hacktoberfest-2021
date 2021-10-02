980e1102629805fde675888c5bfb019b73eeffd7
Hritik Maheshwari <60053891+CRAZYGEEKS04@users.noreply.github.com>
2021-10-03 00:21:23 +0530

    Create hritik.py

---
+++ Submissions/Finding_Middle_Element_In_A_Linked_List/hritik.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findMid(self, head):
+        if head is None:
+            return None
+    
+        ptr1 = head
+        ptr2 = head
+        while(ptr2 is not None and ptr2.next is not None):
+            ptr1 = ptr1.next
+            # this pointer moves 1 nodes ahead everytime loop is run
+        
+            ptr2 = ptr2.next.next
+            # this pointer moves 2 nodes ahead everytime loop is run
+        
+        return ptr1.data
